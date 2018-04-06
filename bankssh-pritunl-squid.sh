a1fea285283bba7c6ba132349e041a6b168695dd
Panuwatbank <35716517+Panuwatbank@users.noreply.github.com>
2018-04-07 01:38:50 +0700

    Update bankssh-pritunl-squid.sh

--- bankssh-pritunl-squid.sh
+++ bankssh-pritunl-squid.sh
@@ -96,6 +96,21 @@ systemctl start pritunl mongod
 systemctl enable pritunl mongod
 sudo service pritunl start
 
+# Install Pritunl [Ubuntu 14.04 x 64bit] 
+cd
+sudo tee -a /etc/apt/sources.list.d/mongodb-org-3.6.list << EOF
+deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.6 multiverse
+EOF
+
+sudo tee -a /etc/apt/sources.list.d/pritunl.list << EOF
+deb http://repo.pritunl.com/stable/apt trusty main
+EOF
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 42F3E95A2C4F08279C4960ADD68FA50FEA312927
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A 
+apt-get update
+apt-get --assume-yes install pritunl mongodb-org
+service pritunl start
+
 
 
 rm /bin/criarusuario /bin/expcleaner /bin/sshlimiter /bin/addhost /bin/listar /bin/sshmonitor /bin/ajuda > /dev/null
@@ -217,11 +232,13 @@ if [[ "$sshcompression" = 's' ]]; then
 fi
 if [[ "$sshcompression" = 'n' ]]; then
 	grep -v "^Compression yes" /etc/ssh/sshd_config > /tmp/sshcp && mv /tmp/sshcp /etc/ssh/sshd_config
+	
 /etc/init.d/squiq restart
 /etc/init.d/squiq3 restart
 /etc/init.d/ssh restart
 /etc/init.d/dropbear restart
 /etc/init.d/dropbear start
+sudo service pritunl restart
 sudo pritunl setup-key
 fi
 exit 1
