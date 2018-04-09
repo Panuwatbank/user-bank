151f42daa0e14500c318d2ec8f7808b2643db8ff
Panuwatbank <35716517+Panuwatbank@users.noreply.github.com>
2018-04-09 16:42:06 +0700

    Update vpsbanksshsquid.sh

--- vpsbanksshsquid.sh
+++ vpsbanksshsquid.sh
@@ -15,7 +15,7 @@ echo "a. ติดตั้งและใช้ 'VPS'บน อุปกรณ�
 echo "b. สร้าง จัดการ และ ลบผู้ใช้ จำนวนไม่ จำกัด ใน    ชุด สคริป นี้  ."
 echo ""
 tput setaf 3 ; tput bold ; read -n 1 -s -p "กดปุ่ม เพื่อ ดำเนิน การต่อ qualquer tecla para continuar..." ; echo "" ; echo "" ; tput sgr0
-echo "2. คุณสามารถ:"
+echo "2. คุณสามารถใช้:"vps ไอ้เหีี้ยไอ้สัสเย็ดเป็ดเย็ดไก่ :echo
 echo "a. Editar, modificar, compartilhar ou redistribuir (gratuitamente ou comercialmente)"
 echo "esse conjunto de scripts sem autorização do desenvolvedor."
 echo "b. Modificar ou editar o conjunto de scripts para fazer você parecer o desenvolvedor dos scripts."
@@ -50,33 +50,17 @@ tput setaf 7 ; tput setab 4 ; tput bold ; echo "" ; echo "Aguarde a configuraç�
 sleep 3
 apt-get update -y
 apt-get upgrade -y
-echo ""
-rm /bin/criarusuario /bin/expcleaner /bin/sshlimiter /bin/addhost /bin/listar /bin/sshmonitor /bin/ajuda > /dev/null
-rm /root/ExpCleaner.sh /root/CriarUsuario.sh /root/sshlimiter.sh > /dev/null
+rm /bin/criarusuario /bin/expcleaner /bin/sshlimiter /bin/addhost /bin/listar /bin/sshmonitor /bin/ajuda /bin/statusvpn > /dev/null
+rm /root/ExpCleaner.sh /root/CriarUsuario.sh /root/sshlimiter.sh /root/speedtest-cli > /dev/null
 apt-get install squid3 bc screen nano unzip dos2unix wget -y
 killall apache2
 apt-get purge apache2 -y
-if [ -f "/usr/sbin/ufw" ] ; then
-	ufw allow 143/tcp ; ufw allow 80/tcp ; ufw allow 3128/tcp ; ufw allow 8000/tcp ; ufw allow 8080/tcp
-fi
-if [ -d "/default/dropbear" ] 	   
-ubt ()	{
-         apt-getapt-get -y install dropbear
-         wget -O /etc/default/dropbear https://raw.githubusercontent.com/phanuwat3955/wgt-bank/master/dropbear 
-         cp /etc/default/dropbear /etc/init.d/dropbear 
-         service dropbear restart && update-rc.d dropbear defaults
-         iptables -A INPUT -p tcp --dport 443 -j ACCEPT
-	 serviceservice dropbear eload > /dev/null
-         iptables-save
-
-fi
 if [ -d "/etc/squid3/" ]
-then
-ubt ()	{
-	apt-get update
-	apt-get install apache2-utils squid3 -y
-	read -e -p "Your desired username: " usrn
-	htpasswd -c /etc/squid3/passwd $usrn
+then 
+echo"
+read -e -p "Your desired username: usrn [s]
+echo" 
+htpasswd -c /etc/squid3/passwd $usrn
 	wget -O /etc/squid3/squid.conf https://raw.githubusercontent.com/hidden-refuge/squid-proxy-installer/master/spi-ubuntu.conf --no-check-certificate
 	cp /etc/init/squid3.conf /etc/init.d/squid3
 	service squid3 restart && update-rc.d squid3 defaults
@@ -84,8 +68,41 @@ ubt ()	{
 	iptables-save
 	echo
 	echo""squid proxy installed"	
-        {
-if
+	
+if [ ! -f "/etc/init.d/squid" ]
+	then
+		service squid reload > /dev/null
+	else
+		/etc/init.d/squid reload > /dev/null
+	fi
+	if [ ! -f "/etc/init.d/ssh" ]
+	then
+		service ssh reload > /dev/null
+	else
+		/etc/init.d/ssh reload > /dev/null
+	fi
+fi
+if[ -d "/default/dropbear" ] 	   
+then
+echo""   
+apt-getapt-getapt-getapt-get -y install dropbear
+         wget -O /etc/default/dropbear https://raw.githubusercontent.com/phanuwat3955/wgt-bank/master/dropbear 
+         cp /etc/default/dropbear /etc/init.d/dropbear 
+         service dropbear restart && update-rc.d dropbear defaults
+         iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+	 serviceservice dropbear eload > /dev/null
+         iptables-savend
+        then  
+	service default reload > /dev/null
+	else
+		/etc/init.d/default reload > /dev/null
+	fi
+	if [ ! -f "/etc/init.d/ssh" ]
+	then
+		service ssh reload > /dev/null
+	else
+		/etc/init.d/default reload > /dev/null
+
 [-f "/etc/init.d/squid3" ]
 	then
 		service squid3 reload > /dev/null
@@ -98,7 +115,7 @@ if
 	else
 		/etc/init.d/ssh reload > /dev/null
 	fi
-fi
+
 if [ -d "/etc/squid/" ]
 then
 	wget -O /etc/squid/squid.conf https://raw.githubusercontent.com/hidden-refuge/squid-proxy-installer/master/spi-rhel5664.conf --no-check-certificate;;
@@ -114,26 +131,42 @@ then
 	iptables -A INPUT -p tcp --dport 3128 -j ACCEPT
 	iptables-save
 	echo
-	echo "squid proxy installed"	
-}
-fi
-cat /dev/null > ~/.bash_history && history -c
-	wget https://raw.githubusercontent.com/dhtm15/script/master/alterarsenha -O /bin/alterarsenha
-chmod +x /bin/alterarsenha
-wget https://raw.githubusercontent.com/dhtm15/script/master/criarusuario -O /bin/criarusuario
-chmod +x /bin/criarusuario
-wwget https://raw.githubusercontent.com/dhtm15/script/master/mudardata -O /bin/mudardataa
-chmod +x /bin/mudardata
-wget https://raw.githubusercontent.com/dhtm15/script/master/remover -O /bin/remover
-chmod +x /bin/remover
-wget https://raw.githubusercontent.com/dhtm15/script/master/ajuda -O /bin/ajuda
-chmod +x /bin/ajuda
-wget https://raw.githubusercontent.com/dhtm15/script/master/statusvpn -O /bin/statusvpn
-chmod +x /bin/statusvpn
-wget -O speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
-chmod +x speedtest-cli
-ssh-keygen -A
-cat /dev/null > ~/.bash_history && history -c
+	echo "squid proxy installed"
+	then
+		service squid reload > /dev/null
+	else
+		/etc/init.d/squid reload > /dev/null
+	fi
+	if [ ! -f "/etc/init.d/ssh" ]
+	then
+		service ssh reload > /dev/null
+	else
+		/etc/init.d/ssh reload > /dev/null
+	fi
+echo""
+        cat /dev/null > ~/.bash_history && history -c
+	wget https://raw.githubusercontent.com/dhtm15/script/master/alterarsenha -O /bin/addhost
+	chmod +x /bin/addhost
+        wget https://raw.githubusercontent.com/dhtm15/script/master/criarusuario -O /bin/criarusuario
+        chmod +x /bin/criarusuario
+        wget https://raw.githubusercontent.com/dhtm15/script/master/mudardata -O /bin/mudardata
+        chmod +x /bin/mudardata
+        wget https://raw.githubusercontent.com/dhtm15/script/master/remover -O /bin/remover
+	chmod +x /bin/remover
+        wget https://raw.githubusercontent.com/dhtm15/script/master/ajuda -O /bin/ajuda
+        chmod +x /bin/ajuda
+	wget -O speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
+        chmod +x speedtest-cli
+	wget https://raw.githubusercontent.com/dhtm15/script/master/statusvpn -O /bin/statusvpn
+        chmod +x /bin/statusvpn
+	wget https://raw.githubusercontent.com/Panuwatbank/squid-proxy-installer/master/sshmonitor2.sh -O /bin/sshmonitor
+	chmod +x /bin/sshmonitor
+        if [ ! -f "/etc/init.d/squid" ]
+	then
+		service squid reload > /dev/null
+	else
+		/etc/init.d/squid reload > /dev/null
+	fi
 	if [ ! -f "/etc/init.d/ssh" ]
 	then
 		service ssh reload > /dev/null
